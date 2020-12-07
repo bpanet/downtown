9452750780830c87db09fe18c8a7f1d4ac263fb2
Alexander Pankow <alexander.pankow@blackpoint.de>
2020-12-07 17:47:40 +0100

    Korrektur der API-URL im Prod-Env

--- src/MerchantFrontend/src/environments/environment.prod.ts
+++ src/MerchantFrontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   hubspotPortalId: '8942616',
   hubspotFormId: '645be48c-f086-48fe-9c83-f85f26a13716',
-  apiUrl: 'http://bad-vilbel.geschaeftscentrum.de',
+  apiUrl: 'https://geschaeftscentrum.de',
   accessKey: 'SWSCVWWZMGLVWVHTSKE5YTI4DW'
 };
